145045e47048fd6f684fd4101aff4f8a563f839c
Gunhee Yi <gunny2676@gmail.com>
2021-04-12 14:29:30 +0900

    Starbucks

--- parse.c
+++ parse.c
@@ -15,16 +15,68 @@
 int text_size;
 int data_size;
 
+char* substr(char* str, int s, int e) {
+	char* new = (char*) malloc(sizeof(char)*(e-s+2));
+	strncpy(new, str+s, e-s+1);
+	new[e-s+1] = 0;
+	return new;
+}
+
 instruction parsing_instr(const char *buffer, const int index)
 {
     instruction instr;
 	/** Implement this function */
+	instr.opcode = fromBinary(substr(buffer, 0, 5));
+
+	printf("%s ", buffer);
+
+	switch(instr.opcode) {
+	    //I format
+	    case 0x9:		//ADDIU
+	    case 0xc:		//ANDI
+	    case 0xf:		//LUI	
+	    case 0xd:		//ORI
+	    case 0xb:		//SLTIU
+	    case 0x23:		//LW	
+	    case 0x2b:		//SW
+	    case 0x4:		//BEQ
+	    case 0x5:		//BNE
+			instr.r_t.r_i.rs = fromBinary(substr(buffer, 6, 10));
+			instr.r_t.r_i.rt = fromBinary(substr(buffer, 11, 15));
+			instr.r_t.r_i.r_i.imm = fromBinary(substr(buffer, 16, 31));
+			// printf("%c %d %d %d %d\n", 'I', instr.opcode, instr.r_t.r_i.rs, instr.r_t.r_i.rt, instr.r_t.r_i.r_i.imm);
+			break;
+
+		//R format
+	    case 0x0:		//ADDU, AND, NOR, OR, SLTU, SLL, SRL, SUBU if JR
+			instr.r_t.r_i.rs = fromBinary(substr(buffer, 6, 10));
+			instr.r_t.r_i.rt = fromBinary(substr(buffer, 11, 15));
+			instr.r_t.r_i.r_i.r.rd = fromBinary(substr(buffer, 16, 20));
+			instr.r_t.r_i.r_i.r.shamt = fromBinary(substr(buffer, 21, 25));
+			instr.func_code = fromBinary(substr(buffer, 26, 31));
+			// printf("%c %d %d %d %d %d %d\n", 'R', instr.opcode, instr.r_t.r_i.rs, instr.r_t.r_i.rt, instr.r_t.r_i.r_i.r.rd, instr.r_t.r_i.r_i.r.shamt, instr.func_code);
+			break;
+
+		//J format
+	    case 0x2:		//J
+	    case 0x3:		//JAL
+			instr.r_t.target = fromBinary(substr(buffer, 6, 31));
+			// printf("%c %d %d\n", 'J', instr.opcode, instr.r_t.target);
+			break;
+
+	    // default:
+		// printf("Not available instruction\n");
+		// assert(0);
+	}
+
+
     return instr;
 }
 
 void parsing_data(const char *buffer, const int index)
 {
 	/** Implement this function */
+	mem_write_32(MEM_REGIONS[0].size + index, fromBinary(buffer));
 }
 
 void print_parse_result()
--- run.c
+++ run.c
@@ -33,4 +33,64 @@ instruction* get_inst_info(uint32_t pc)
 /***************************************************************/
 void process_instruction(){
 	/** Implement this function */
+    instruction instr = *get_inst_info(CURRENT_STATE.PC);
+
+    switch(instr.opcode)
+	{
+	    //I format
+	    case 0x9:		//ADDIU
+	    case 0xc:		//ANDI
+	    case 0xf:		//LUI	
+	    case 0xd:		//ORI
+	    case 0xb:		//SLTIU
+	    case 0x23:		//LW	
+	    case 0x2b:		//SW
+	    case 0x4:		//BEQ
+	    case 0x5:		//BNE
+		break;
+
+    	    //R format
+	    case 0x0:		//ADDU, AND, NOR, OR, SLTU, SLL, SRL, SUBU if JR
+            switch(instr.func_code) {
+                case 0x21:  //ADDU
+
+                    break;
+                case 0x24: //AND
+
+                    break;
+                case 0x08: //JR
+                    
+                    break;
+                case 0x27: //NOR
+
+                    break;
+                case 0x25: //OR
+
+                    break;
+                case 0x2b: //SLTU
+
+                    break;
+                case 0x00: //SLL
+                    
+                    break;
+                case 0x02: //SRL
+
+                    break;
+                case 0x23: //SUBU
+                    
+                    break;
+            }
+		break;
+
+    	    //J format
+	    case 0x2:		//J
+	    case 0x3:		//JAL
+		printf("INST_INFO[%d].target : %d\n",i, INST_INFO[i].r_t.target);
+		break;
+
+	    default:
+		printf("Not available instruction\n");
+		assert(0);
+	}
+
 }
